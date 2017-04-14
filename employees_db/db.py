41a4b257a73714ddb4037137638519836a59fe9b
AutoRun <osmstas@gmail.com>
2017-04-14 18:48:10 +0300

    minor fix

--- employees_db/db.py
+++ employees_db/db.py
@@ -4,7 +4,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        # 'HOST': 'localhost',
         'USER': 'employees_db_user',
         'PASSWORD': 'employee0',
         'NAME': 'employees_db',
@@ -13,8 +12,5 @@ DATABASES = {
         'OPTIONS': {
             'init_command': 'SET default_storage_engine=INNODB',
         }
-    },
-    # 'OPTIONS': {
-    #    'init_command': 'SET default_storage_engine=XTRADB',
-    # }
+    }
  }
